a5496a03d321635b4396a3e03cf52c07339bd0fd
joshcrasto <jcrasto@g.ucla.edu>
2020-08-08 19:38:05 -0500

    pawn implementation

--- src/main/java/pieces/Pawn.java
+++ src/main/java/pieces/Pawn.java
@@ -3,5 +3,24 @@ package pieces;
 import gamePlay.Board;
 import gamePlay.Square;
 
-public class Pawn {
+public class Pawn extends Piece{
+    public Pawn(boolean white){
+        super(white);
+    }
+    @Override
+    public boolean canMove(Board board, Square start, Square end){
+        if (end.getPiece().isWhite() == this.isWhite()){
+            return false;
+        }
+        int x = end.getX() - start.getX();
+        int y = Math.abs(start.getY() - end.getY());
+
+        if (x == 1 && y == 0){
+            return true;
+        }
+        else{
+            //TODO if can take return true
+            return false;
+        }
+    }
 }
