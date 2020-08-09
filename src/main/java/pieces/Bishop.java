77124b8125bb5d42f40c372fea6d9896018af2ee
joshcrasto <jcrasto@g.ucla.edu>
2020-08-08 22:03:19 -0500

    implementation of the different pieces

--- src/main/java/pieces/Bishop.java
+++ src/main/java/pieces/Bishop.java
@@ -1,4 +1,26 @@
 package pieces;
 
-public class Bishop {
+import gamePlay.Board;
+import gamePlay.Square;
+
+
+public class Bishop extends Piece{
+    public Bishop(boolean white){
+        super(white);
+    }
+    @Override
+    public boolean canMove(Board board, Square start, Square end){
+        if (end.getPiece().isWhite() == this.isWhite()){
+            return false;
+        }
+        int x = Math.abs(start.getX() - end.getX());
+        int y = Math.abs(start.getY() - end.getY());
+
+        if ( x != 0 && y != 0){
+            if(x/y == 1){
+                return true;
+            }
+        }
+        return false;
+    }
 }
--- src/main/java/pieces/Rook.java
+++ src/main/java/pieces/Rook.java
@@ -1,4 +1,25 @@
 package pieces;
 
-public class Rook {
+import gamePlay.Board;
+import gamePlay.Square;
+
+public class Rook extends Piece {
+    public Rook(boolean white){
+        super(white);
+    }
+    @Override
+    public boolean canMove(Board board, Square start, Square end){
+        if (end.getPiece().isWhite() == this.isWhite()){
+            return false;
+        }
+        int x = Math.abs(start.getX() - end.getX());
+        int y = Math.abs(start.getY() - end.getY());
+        if (x > 0 && x < 8 && y ==0){
+            return true;
+        }
+        else if (x == 0 && y > 0 && y < 8){
+            return true;
+        }
+        return false;
+    }
 }
