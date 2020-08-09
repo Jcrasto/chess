053b9ed8145e54020d79a30148a813e6224d0dd1
joshcrasto <jcrasto@g.ucla.edu>
2020-08-09 16:06:45 -0500

    creating game class and enums for the status of the game

---
+++ src/main/java/gamePlay/Game.java
@@ -0,0 +1,15 @@
+package gamePlay;
+
+import java.util.List;
+
+public class Game {
+    private Player[] players;
+    private Board board;
+    private Player currentTurn;
+    private GameStatus status;
+    private List<Move> movesPlayed;
+
+    private void intitialize(Player p1, Player p2) {
+
+    }
+}
---
+++ src/main/java/gamePlay/GameStatus.java
@@ -0,0 +1,9 @@
+package gamePlay;
+
+public enum GameStatus{
+    ACTIVE,
+    BLACK_WIN,
+    WHITE_WIN,
+    RESIGNATION,
+    STALEMATE
+}
