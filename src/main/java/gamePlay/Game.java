f7c45c34bc6f4054e7a42e4946724fe9f3a31be8
joshcrasto <jcrasto@g.ucla.edu>
2020-10-25 16:59:43 -0500

    removing extra code

--- src/main/java/gamePlay/Game.java
+++ src/main/java/gamePlay/Game.java
@@ -116,51 +116,5 @@ public class Game {
         }
 
     }
-//        Square startSquare = board.getSquare(startX, startY);
-//        Square endSquare = board.getSquare(endX, endY);
-//        Move move = new Move(player, startSquare, endSquare);
-//        return this.makeMove(move, player);
-//    }
-//
-//    private boolean makeMove(Move move, Player player){
-//        Piece sourcePiece = move.getStart().getPiece();
-//        if (sourcePiece == null){
-//            return false;
-//        }
-//
-//        if (player != currentTurn){
-//            return false;
-//        }
-//
-//        if (sourcePiece.isWhite() != player.isWhiteSide()){
-//            return false;
-//        }
-//
-//        if(!sourcePiece.canMove(board, move.getStart(), move.getEnd())){
-//            return false;
-//        }
-//
-//        Piece destPiece = move.getEnd().getPiece();
-//        if (destPiece != null){
-//            destPiece.setCaptured(true);
-//        }
-//
-//        if (sourcePiece != null && sourcePiece instanceof King){
-//            if (player.isWhiteSide()){
-//                this.setStatus(GameStatus.WHITE_WIN);
-//            }
-//            else{
-//                this.setStatus(GameStatus.BLACK_WIN);
-//            }
-//        }
-//
-//        if (this.currentTurn == players[0]){
-//            this.currentTurn = players[1];
-//        }
-//        else {
-//            this.currentTurn = players[0];
-//        }
-//        return true;
-//    }
 
 }
