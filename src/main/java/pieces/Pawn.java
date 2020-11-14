5154261a8f87ce43b4d5eb99875badda50c1146d
joshcrasto <jcrasto@g.ucla.edu>
2020-11-14 15:51:43 -0600

    canmove function for pawns

--- src/main/java/pieces/Pawn.java
+++ src/main/java/pieces/Pawn.java
@@ -1,6 +1,5 @@
 package pieces;
 
-import gamePlay.Board;
 import gamePlay.Square;
 
 public class Pawn extends Piece {
@@ -17,14 +16,31 @@ public class Pawn extends Piece {
 
     @Override
     public boolean canMove(Square[][] squares,int x1, int y1, int x2, int y2) {
-//        if (end.getPiece().isWhite() == this.isWhite()) {
-//            return false;
-//        }
-//        int x = end.getX() - start.getX();
-//        int y = Math.abs(start.getY() - end.getY());
-//
-//        //TODO if can take return true
-//        return x == 1 && y == 0;
+        if (squares[x2][y2].getPiece().isWhite() == this.isWhite()){
+            return false;
+        }
+        if (x2 - x1 == 0){
+            if (squares[x2][y2].getPiece() != null){
+                return false;
+            } else if (y2 - y1 == 2){
+                if (this.isWhite()){
+                    if (y1 == 1){
+                        return true;
+                    }
+                } else {
+                    if (y1 == 6){
+                        return true;
+                    }
+                }
+                return false;
+            }
+        } else if (Math.abs(x2 - x1) == 1){
+            if (squares[x2][y2].getPiece().isWhite() != this.isWhite() && y2 - y1 == 1){
+                return true;
+            }
+        } else {
+            return false;
+        }
         return true;
     }
 }
