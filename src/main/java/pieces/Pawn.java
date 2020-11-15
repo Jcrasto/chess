0e22a552cf621af9eb3cf3416c87184d71af5292
joshcrasto <jcrasto@g.ucla.edu>
2020-11-14 20:48:17 -0600

    adding functionality to make sure pawns only move in one direction

--- src/main/java/pieces/Pawn.java
+++ src/main/java/pieces/Pawn.java
@@ -17,20 +17,55 @@ public class Pawn extends Piece {
     public boolean capturingMove(Square[][] squares, int x1, int y1, int x2, int y2) {
         if (squares[x2][y2].getPiece() == null) {
             return false;
-        } else if (squares[x2][y2].getPiece().isWhite() != this.isWhite() && y2 - y1 == 1) {
-            return true;
-        } else {
-            return false;
+        } else if (squares[x2][y2].getPiece().isWhite() != this.isWhite()) {
+            if (this.isWhite()){
+                //check if moving the right direction
+                if (y2 - y1 == 1){
+                    return true;
+                }
+            } else {
+                //check if moving the right direction
+                if (y1 - y2 == 1){
+                    return true;
+                }
+            }
         }
+        return false;
     }
 
-    public boolean twoSquareMove(Square[][] squares, int x1, int y1, int x2, int y2) {
+    public boolean twoSquareMove(Square[][] squares,int x1, int y1, int y2) {
         if (this.isWhite()) {
             if (y1 == 1) {
-                return true;
+                //check if moving the right direction
+                if (y2 - y1 == 2) {
+                    //check if any piece is blocking
+                    if(squares[x1][y1 + 1].getPiece() == null){
+                        return true;
+                    }
+                }
             }
         } else {
             if (y1 == 6) {
+                //check if moving the right direction
+                if (y1 - y2 == 2) {
+                    //check if any piece is blocking
+                    if(squares[x1][y1 -1].getPiece() == null){
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+    public boolean oneSquareMove(Square[][] squares, int y1, int y2) {
+        if (this.isWhite()) {
+            //check if moving the right direction
+            if (y2 > y1) {
+                return true;
+            }
+        } else {
+            //check if moving the right direction
+            if (y1 > y2) {
                 return true;
             }
         }
@@ -47,10 +82,10 @@ public class Pawn extends Piece {
         if (x2 - x1 == 0) {
             if (squares[x2][y2].getPiece() != null) {
                 return false;
-            } else if (y2 - y1 == 2) {
-                return twoSquareMove(squares, x1, y1, x2, y2);
-            } else if (y2 - y1 == 1) {
-                return true;
+            } else if (Math.abs(y2 - y1) == 2) {
+                return twoSquareMove(squares, x1, y1, y2);
+            } else if (Math.abs(y2 - y1) == 2) {
+                return oneSquareMove(squares, y1, y2);
             } else {
                 return false;
             }
--- src/test/java/pieces/PawnTest.java
+++ src/test/java/pieces/PawnTest.java
@@ -20,4 +20,20 @@ public class PawnTest {
         }
         game.playerMove("d2 d4");
     }
+
+    @Test
+    public void QueensGambitTest() throws Exception{
+        Game game = new Game();
+        Player white = new Player(true);
+        Player black = new Player(false);
+        game.initialize(white, black);
+        game.setStatus(GameStatus.WHITE_TURN);
+        game.playerMove("d2 d4");
+        game.setStatus(GameStatus.BLACK_TURN);
+        game.playerMove("d7 d5");
+        game.setStatus(GameStatus.WHITE_TURN);
+        game.playerMove("c2 c4");
+
+
+    }
 }
