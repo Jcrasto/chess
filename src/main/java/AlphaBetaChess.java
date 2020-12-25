29caf69ae62539aeb0e43ff409f6884a2dcdc410
joshcrasto <jcrasto@g.ucla.edu>
2020-12-24 18:36:32 -0800

    implementing queen moves (tutorial 9)

--- src/main/java/AlphaBetaChess.java
+++ src/main/java/AlphaBetaChess.java
@@ -73,11 +73,45 @@ public class AlphaBetaChess {
 
     public static String possibleQ(int i) {
         String list = "";
+        String oldPiece;
+        int r = i / 8;
+        int c = i % 8;
+        int temp = 1;
+        for (int j = -1; j <= 1; j++) {
+            for (int k = -1; k <= 1; k++) {
+                try {
+                    while (" ".equals(chessBoard[r + temp * j][c + temp * k])) {
+                        oldPiece = chessBoard[r + temp * j][c + temp * k];
+                        chessBoard[r][c] = " ";
+                        chessBoard[r + temp * j][c + temp * k] = "Q";
+                        if (kingSafe()) {
+                            list = list + r + c + (r + temp * j) + (c + temp * k) + oldPiece;
+                        }
+                        chessBoard[r][c] = "Q";
+                        chessBoard[r + temp * j][c + temp * k] = oldPiece;
+                        temp++;
+                    }
+                    if (Character.isLowerCase(chessBoard[r + temp * j][c + temp * k].charAt(0))) {
+                        oldPiece = chessBoard[r + temp * j][c + temp * k];
+                        chessBoard[r][c] = " ";
+                        chessBoard[r + temp * j][c + temp * k] = "Q";
+                        if (kingSafe()) {
+                            list = list + r + c + (r + temp * j) + (c + temp * k) + oldPiece;
+                        }
+                        chessBoard[r][c] = "Q";
+                        chessBoard[r + temp * j][c + temp * k] = oldPiece;
+                    }
+                } catch (Exception e) {
+                }
+                temp = 1;
+            }
+        }
         return list;
     }
 
     public static String possibleK(int i) {
-        String list = "", oldPiece;
+        String list = "";
+        String oldPiece;
         int r = i / 8;
         int c = i % 8;
         for (int j = 0; j < 9; j++) {
